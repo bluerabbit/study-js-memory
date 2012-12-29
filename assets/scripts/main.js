5a6afd481df7d19f41f62a8638801f63e0a6e3fc
Akira Kusumoto <akirakusumo10@gmail.com>
2012-12-29 23:39:45 +0900

    メモリー・リーク 3: 循環参照

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,18 @@
-var leak;
-
 $("#start_button").click(function () {
+    var leakExists = !(
+        window["leak"] === null || window["leak"] === undefined
+        );
+    if (leakExists) {
+        return;
+    }
     leak = new Leaker();
-    leak.init();
+    leak.init("leaker 1", null, registry);
 });
 
-$("#destroy_button").click(function(){
+$("#destroy_button").click(function () {
     leak.destroy();
     leak = null;
 });
 
-
+registry = new Registry();
+registry.init();
